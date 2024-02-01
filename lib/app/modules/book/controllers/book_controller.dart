bc8e618a9ca4fcacab34bcdfc76debc7b71a9477
totokxinacoy <kristianwiranata19@gmail.com>
2024-02-01 13:57:39 +0700

    tugas22

--- lib/app/modules/book/controllers/book_controller.dart
+++ lib/app/modules/book/controllers/book_controller.dart
@@ -1,9 +1,17 @@
-import 'package:get/get.dart';
+import 'dart:math';
 
-class BookController extends GetxController {
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_kelas_c/app/data/constant/endpoint.dart';
+import 'package:peminjam_perpustakaan_kelas_c/app/data/model/response_book.dart';
+import 'package:peminjam_perpustakaan_kelas_c/app/data/provider/api_provider.dart';
+
+
+class BookController extends GetxController with StateMixin<List<DataBook>> {
   //TODO: Implement BookController
 
   final count = 0.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -12,11 +20,39 @@ class BookController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    GetData();
   }
 
   @override
   void onClose() {
     super.onClose();
+  }
+
+  GetData() async {
+    change(null, status: RxStatus.loading());
+    try {
+      final response = await ApiProvider.instance().get(Endpoint.book);
+      if (response.statusCode == 200) {
+        final ResponseBook responseBook = ResponseBook.fromJson(response.data);
+        if (responseBook.data!.isEmpty) {
+          change(null, status: RxStatus.empty());
+        } else {
+          change(responseBook.data, status: RxStatus.success());
+        }
+      } else {
+        change(null, status: RxStatus.error("Gagal mengambil data"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(null, status: RxStatus.error(" dio ${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error(e.message ?? ""));
+      }
+    } catch (e) {
+      change(null, status: RxStatus.error(e.toString()));
+    }
   }
 
   void increment() => count.value++;
--- lib/app/modules/book/views/book_view.dart
+++ lib/app/modules/book/views/book_view.dart
@@ -1,24 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_kelas_c/app/data/model/response_book.dart';
+import 'package:peminjam_perpustakaan_kelas_c/app/routes/app_pages.dart';
 
 import '../controllers/book_controller.dart';
 
 class BookView extends GetView<BookController> {
   const BookView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BookView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'BookView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('BookView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: controller.obx(
+            (state) => ListView.separated(
+                  itemCount: state!.length,
+                  itemBuilder: (context, index) {
+                    DataBook dataBook = state[index];
+                    return ListTile(
+                      leading: Icon(FontAwesomeIcons.bookQuran),
+                      title: Text("${dataBook.judul}"),
+                      subtitle: Text(
+                          "Penulis ${dataBook.penulis}\n${dataBook.penerbit} - ${dataBook.tahunTerbit}"),
+                      trailing: ElevatedButton(
+                        onPressed: () => Get.toNamed(Routes.ADD_PEMINJAMAN,
+                            parameters: {
+                              'id': (dataBook.id ?? 0).toString(),
+                              'judul': dataBook.judul ?? '-'
+                            }),
+                        child: Text("Pinjam"),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => Divider(),
+                ),
+            onLoading: Center(child: CupertinoActivityIndicator())));
   }
 }
