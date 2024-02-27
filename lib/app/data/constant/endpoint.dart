3b0b4971bd82a757489b0d6a2b8c344e7f1a164c
Fushigo <rizky23ska@gmail.com>
2024-02-27 10:43:14 +0700

    tugas

--- lib/app/data/constant/endpoint.dart
+++ lib/app/data/constant/endpoint.dart
@@ -1,5 +1,5 @@
 class Endpoint {
-  static const String baseUrlApi = "https://perpustakaan.ppksbogor.com/api/v1/user/";
+  static const String baseUrlApi = "https://192.168.6.27:8000/api/v1/user/";
 
 
   static const String register = "${baseUrlApi}register";
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,47 +9,74 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
+      backgroundColor: Color(0xFF181820),
       body: Center(
-          child: Form(key: controller.formKey, child: Column(
-            children: [
-              TextFormField(
-                  controller: controller.usernameController,
-                  decoration: InputDecoration(hintText: "Masukan Username"),
-                  validator: (value) {
-                    if (value!.length < 2) {
-                      return "username tidak boleh kosong";
-                    }
-                    return null;
-                  }
-              ),
-              TextFormField(
-                  controller: controller.passwordController,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: "Masukan Password"),
-                  validator: (value) {
-                    if (value!.length < 2) {
-                      return "password tidak boleh kosong";
-                    }
-                    return null;
-                  }
-              ),
-              Obx(() =>
-              controller.loading.value
-                  ? CircularProgressIndicator()
-                  : ElevatedButton(onPressed: () {
-                controller.login();
-              }, child: Text("Login"))),
-              ElevatedButton(onPressed: ()=>Get.toNamed(Routes.REGISTER),
-                  child: Text("Open Register"))
-            ],
-          ),)
+        child: Padding(
+          padding: const EdgeInsets.all(80.0),
+          child: Form(
+              key: controller.formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/marnoski.png',
+                      width: 400,
+                      height: 400,
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                        controller: controller.usernameController,
+                        decoration: InputDecoration(
+                          hintText: "Masukan Username",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.length < 2) {
+                            return "Username tidak boleh kosong";
+                          }
+                          return null;
+                        }),
+                    SizedBox(height: 15.0),
+                    TextFormField(
+                        controller: controller.passwordController,
+                        decoration: InputDecoration(
+                          hintText: "Masukan Passoword",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.length < 2) {
+                            return "Password tidak boleh kosong";
+                          }
+                          return null;
+                        }),
+                    SizedBox(height: 25.0),
+            Row (
+                children : [
+                  Obx(() => controller.loading.value
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                      onPressed: () {
+                        controller.login();
+                      },
+                      child: Text("Login"))),
+                  SizedBox(width: 540),
+                  SizedBox(height: 15.0),
+                  ElevatedButton(onPressed : ()=>Get.toNamed(Routes.REGISTER), child: Text("Open Register")),
+                ]
+            ),
+            ]
+          ),
+        ),
       ),
-    );
+    ),
+    );1;
   }
 }
